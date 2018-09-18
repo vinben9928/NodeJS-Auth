594b74021e0739e8795e0eb4902ab73397cbf69d
Vincent Bengtsson <vinben9928@edu.halmstad.se>
2018-09-18 19:20:58 +0200

    Implemented the new 'auth' module.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const validateUser = require("./modules/validateUser");
+const auth = require("./modules/auth");
 
 const app = express();
 const port = 8080;
@@ -20,14 +20,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.post("/login", function(request, response) {
     const user = JSON.parse(request.body.user);
-
     
+    auth.loginAsync(user.email, user.password).then(function(token) {
+        response.send(JSON.stringify({ access_token: token }));
+    }).catch(function(error) {
+        response.send(error);
+    });
 });
 
 app.post("/register", function(request, response) {
     var user = JSON.parse(request.body.user);
 
-    
+    auth.createUserAsync(user.email, user.password).then(function(result) {
+        if(result === true) {
+            response.send("User created successfully!");
+        }
+    }).catch(function(error) {
+        response.send(error);
+    });
 });
 
 app.listen(port, () => {
